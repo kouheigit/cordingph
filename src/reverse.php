c6959b79a17266b24f43c57294ef37a8c7e18240
kouheigit <ookawakouhei777@gmail.com>
2024-05-29 20:02:05 +0900

    dev6

--- src/reverse.php
+++ src/reverse.php
@@ -1,7 +1,22 @@
 <?php
-function reverse(){
-    
+function palindrome($num_before){
+
+    //strval整数に変換
+    //文字列をstrrev反転
+    $num = strval($num_before);
+
+    $num_reverse = strrev($num);
+
+    if($num == $num_reverse){
+        echo "true";
+    }else{
+        echo"false";
+    }
 }
+$value = 111;
+$ans = palindrome($value);
+print $ans;
+
 /*
 function isPalindrome($num) {
     // 数値を文字列に変換
