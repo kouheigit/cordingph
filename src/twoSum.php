df7c82cacfb41dd3ae47d16e55f6f12ff59346a0
kouheigit <ookawakouhei777@gmail.com>
2024-05-29 20:33:04 +0900

    dev10

--- src/twoSum.php
+++ src/twoSum.php
@@ -9,18 +9,5 @@ $num = [100,200,300,400];
 $target = "ナメック";
 $result = twoSum($num,$target);
 echo $result;
-/*
-function twoSum($num,$target)
-{
-    //countでの出力
-    for ($i=0; $i < count($num); $i++) {
-        echo $num[$i];
-    }
-    echo $target;
-}
 
-$num = [10,11,12,13];
-$target = "これは配列の出力結果です";
-$result1 = twoSum($num,$target);
-echo $result1;*/
 ?>
