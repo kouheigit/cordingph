df42627d91bc92c890b1042cb2749c6f9541646f
kouheigit <ookawakouhei777@gmail.com>
2024-05-30 20:58:53 +0900

    dev12

--- src/twosum1.php
+++ src/twosum1.php
@@ -7,6 +7,23 @@ function twosum1($num){
         echo"<BR>";
     }
 }
+function palindrom($str){
+    //string型に戻す
+    $str_string = strval($str);
+    $str_reverse = strrev($str_string);
+    if($str_string == $str_reverse){
+        echo "true";
+    }else{
+        echo"false";
+    }
+}
+//palindrom
+$test = 111;
+$strreslt = palindrom($test);
+echo $strreslt;
+
+//twosum
+//twosum1
 $hoge = [100,200,300];
 $result = twosum1($hoge);
 print_r($result);
