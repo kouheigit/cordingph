853ba2c4a3262fc4f1dda0dfb22f7e819fc55883
kouheigit <ookawakouhei777@gmail.com>
2024-06-01 18:06:51 +0900

    dev20

--- src/twoSum2.php
+++ src/twoSum2.php
@@ -1,9 +1,17 @@
 <?php
 class twoSum{
-    public function Sum(){
-        
+    public function Sum(array $num){
+        $sum = 0;
+        for($i=0; $i<count($num); $i++){
+            $sum = sum + $num[$i];
+        }
+        echo $sum;
     }
 }
+
+$twoSum = new twoSum();
+$answer = $twoSum->Sum([1,2,3]);
+echo $answer;
 /*
 $problem = new problem();
 $ans = $problem->number42([3, 2, 1]);
