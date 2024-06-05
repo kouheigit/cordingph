47214ec6c62aa5eaa297895426e6aa20fb42aec9
kouheigit <ookawakouhei777@gmail.com>
2024-06-05 14:12:44 +0900

    dev27

--- src/twoSum.php
+++ src/twoSum.php
@@ -8,8 +8,7 @@ function twoSum($nums, $target) {
         // 必要な補完数値を計算
         $complement = $target - $nums[$i];
 
-        var_dump($complement);
-        die;
+        
         // 補完数値が連想配列のキーとして存在するかチェック
         //
 
