7a47cddc09a3f99acb9b09886649de1db0ef5984
kouheigit <ookawakouhei777@gmail.com>
2024-06-05 16:55:55 +0900

    dev32

--- src/twosum3.php
+++ src/twosum3.php
@@ -1,20 +1,26 @@
 <?php
 
 class twoSum3{
-    public function method(array $num){
-        $sum = 0;
+    public function method($target,array $num){
+        $map = [];
         for($i=0; $i<count($num); $i++){
-
-            $sum = $sum + $num[$i];
+           $sum =  $target - $num[$i];
+           if(isset($map[$sum])){
+               return [$map[$sum], $i];
+           }
         }
 
-        echo $sum;
     }
 }
 $twoSum3 = new twoSum3();
-$answer =$twoSum3->method([3,4,5,4]);
+$target = 11;
+$num = [3,4,5,6];
+$answer = $twoSum3->method($target,$num);
 echo $answer;
 
+
+/*$answer =$twoSum3->method([3,4,5,4]);
+echo $answer;*/
 /*
  * 整数からなる配列numsとある整数targetが与えられる．
 このとき，足し合わせるとtargetになる２つの数のインデクスを返せ．
