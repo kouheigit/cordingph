4feca6dc0cf1829c401bc34416a6e8bab51fa8e5
kouheigit <ookawakouhei777@gmail.com>
2024-06-05 15:41:03 +0900

    dev31

--- src/twosum3.php
+++ src/twosum3.php
@@ -2,9 +2,24 @@
 
 class twoSum3{
     public function method(array $num){
+        $sum = 0;
         for($i=0; $i<count($num); $i++){
-            echo $num[$i];
+
+            $sum = $sum + $num[$i];
         }
+
+        echo $sum;
     }
 }
+$twoSum3 = new twoSum3();
+$answer =$twoSum3->method([3,4,5,4]);
+echo $answer;
+
+/*
+ * 整数からなる配列numsとある整数targetが与えられる．
+このとき，足し合わせるとtargetになる２つの数のインデクスを返せ．
+
+ただし，各入力はただ一つの解が存在し，同じ要素を2回使うことは出来ないものとする．
+ */
+
 
