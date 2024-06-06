d41c3e1bf122f697b53270b0e9bfe4e518e2708d
kouheigit <ookawakouhei777@gmail.com>
2024-06-06 14:53:52 +0900

    dev43

---
+++ src/twoSum3.php
@@ -0,0 +1,29 @@
+<?php
+
+class twoSum3 {
+    public function method($nums, $target) {
+        // 値とそのインデックスを保持する連想配列
+        $map = [];
+
+        // 配列を走査
+        for ($i = 0; $i < count($nums); $i++) {
+            // 必要な補完数値を計算
+            $complement = $target - $nums[$i];
+
+
+            // 補完数値が連想配列のキーとして存在するかチェック
+            //
+
+            if (isset($map[$complement])) {
+                // 存在する場合、解を返す
+                return [$map[$complement], $i];
+            }
+
+            // 連想配列に値とそのインデックスを保存
+            $map[$nums[$i]] = $i;
+        }
+
+        // 解が存在しない場合（ただし問題の制約上、この行は実行されないはず）
+        return [];
+    }
+}
